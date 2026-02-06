020f3d0091bf521c8d11912906b1448f35489ca0
Shreyash Shetty <157374218+shreyashshetty18@users.noreply.github.com>
2026-02-06 18:21:04 +0530

    Dock Icons

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import "./dock.scss"
 
 const Dock = () => {
   return (
-    <div>Dock</div>
+    <footer className='dock'>
+      <div className="icon">
+        <img src="/doc-icons/github.svg" alt="GitHub" />
+        <img src="/doc-icons/note.svg" alt="Note" />
+        <img src="/doc-icons/pdf.svg" alt="PDF" />
+        <img src="/doc-icons/calender.svg" alt="Calender" />
+        <img src="/doc-icons/spotify.svg" alt="Spotify" />
+        <img src="/doc-icons/mail.svg" alt="Mail" />
+        <img src="/doc-icons/link.svg" alt="Link" />
+        <img src="/doc-icons/cli.svg" alt="CLI" />
+      </div>
+    </footer>
   )
 }
 
