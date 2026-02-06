8a8bb8fb7a978f6bc1d2b02d0ab4dfd4dc2f87c1
Shreyash Shetty <157374218+shreyashshetty18@users.noreply.github.com>
2026-02-06 12:43:38 +0530

    Dock Comp1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import "./app.scss"
-
+import Dock from './components/Dock'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <main />
+      <main>
+        <Dock />
+      </main>
     </>
   )
 }
