03b3c99d7e16336aaed4ccfcfe47cac558289a8d
Shreyash Shetty <157374218+shreyashshetty18@users.noreply.github.com>
2026-02-06 18:42:25 +0530

    Dock Styling

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -3,16 +3,14 @@ import "./dock.scss"
 const Dock = () => {
   return (
     <footer className='dock'>
-      <div className="icon">
-        <img src="/doc-icons/github.svg" alt="GitHub" />
-        <img src="/doc-icons/note.svg" alt="Note" />
-        <img src="/doc-icons/pdf.svg" alt="PDF" />
-        <img src="/doc-icons/calender.svg" alt="Calender" />
-        <img src="/doc-icons/spotify.svg" alt="Spotify" />
-        <img src="/doc-icons/mail.svg" alt="Mail" />
-        <img src="/doc-icons/link.svg" alt="Link" />
-        <img src="/doc-icons/cli.svg" alt="CLI" />
-      </div>
+      <div className="icon github"><img src="/doc-icons/github.svg" alt="GitHub" /></div>
+      <div className="icon note"><img src="/doc-icons/note.svg" alt="Note" /></div>
+      <div className="icon pdf"><img src="/doc-icons/pdf.svg" alt="PDF" /></div>
+      <div className="icon calender"><img src="/doc-icons/calender.svg" alt="Calender" /></div>
+      <div className="icon spotify"><img src="/doc-icons/spotify.svg" alt="Spotify" /></div>
+      <div className="icon mail"><img src="/doc-icons/mail.svg" alt="Mail" /></div>
+      <div className="icon link"><img src="/doc-icons/link.svg" alt="Link" /></div>
+      <div className="icon cli"><img src="/doc-icons/cli.svg" alt="CLI" /></div>
     </footer>
   )
 }
