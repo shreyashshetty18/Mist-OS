116208cd3674b9ab4ef8f4af25ef5ba1302b823b
Shreyash Shetty <157374218+shreyashshetty18@users.noreply.github.com>
2026-02-08 08:31:38 +0530

    NavBar Icons

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,18 @@ import './nav.scss'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div>
+      <div className="left">
+        <div className='apple-logo'>
+          <img src="./navbar-icons/apple.svg" alt="apple logo" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wifi-icon">
+          <img src="./navbar-icons/wifi.svg" alt="wifi icon" />
+        </div>
+      </div>
+    </div>
   )
 }
 
