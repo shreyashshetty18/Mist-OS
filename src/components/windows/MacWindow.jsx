00afff3764aaa02cc35ec968173fc42a479397ab
Shreyash Shetty <157374218+shreyashshetty18@users.noreply.github.com>
2026-02-12 07:59:40 +0530

    RND 2

--- src/components/windows/MacWindow.jsx
+++ src/components/windows/MacWindow.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
 import { Rnd } from 'react-rnd'
+import './windows.scss'
 
 const MacWindow = () => {
   return (
     <Rnd>
-        
+        <div className="window">
+          <div className="nav">
+            <div className="dots">
+              <div className="dot red"></div>
+              <div className="dot yellow"></div>
+              <div className="dot green"></div>
+            </div>
+          </div>
+          <div className="main-content"></div>
+        </div>
     </Rnd>
   )
 }
